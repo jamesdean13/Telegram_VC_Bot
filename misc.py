703675df4c3c80668389f30ce55fe157f75be749
jamesdean13 <65916746+jamesdean13@users.noreply.github.com>
2021-03-23 02:44:47 +0530

    Update misc.py

--- misc.py
+++ misc.py
@@ -11,30 +11,7 @@ HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
 __**NOTE: Do Not Assign These Commands To Bot Via BotFather.**__"""
 
 START_TEXT = """I am Swifties Music Bot, a bot that lets you play music in @Swiftiesworld voice chat.
-This bot is created by @TayLife. 
-Use the buttons below to know more about me.""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "⚒ Creator", url="https://t.me/taylife"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "💬 Group", url="https://t.me/swiftiesworld"
-                    ),
-                    InlineKeyboardButton(
-                        "Channel 🔈", url="https://t.me/taylorswift13fanpage"
-                    ),
-                    InlineKeyboardButton(
-                        "Discography 😈", url="https://t.me/taylorflac"
-
-                    )
-                ]
-            ]
-        )
-    )
+This bot is created by @TayLife.Use"""
 
 REPO_TEXT = "[Group](t.me/swiftiesworld)" \
             + " | [Group](t.me/taylorswift13fanpage)"
